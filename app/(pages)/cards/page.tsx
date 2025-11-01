78d15436e98fcf9f24f6b69ed8975815d3868a3c
DimensionCoin <nick.vassallo97@gmail.com>
2025-11-01 12:01:38 -0400

    blockers

--- app/(pages)/cards/page.tsx
+++ app/(pages)/cards/page.tsx
@@ -1,9 +1,10 @@
+import Blocker from '@/components/shared/Blocker'
 import React from 'react'
 
 const Cards = () => {
   return (
     <div className='py-3 px-4'>
-      Cards are coming soon!!
+      <Blocker/>
     </div>
   )
 }
